400d114a8bdfcf6e7de778d1ab97ff1dc07bb271
Bashir Alansary <101013960+Bashir-Alansary@users.noreply.github.com>
2024-03-10 20:27:54 +0200

    add to first branch

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'jquery/dist/jquery.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 import {ShopContextProvider} from "./Context/ShopContext";
